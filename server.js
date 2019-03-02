0e14fe10130df83143b96ccaee300294e206495b
Adam Hosman <github@adamsinbox.com>
2019-03-02 23:04:22 +0100

    Update CORS handling

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const { database } = require('./database');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 
 const app = express();
 
 
+app.use(cors());
+
+
 app.use(bodyParser.json());
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 app.get('/', (req, res) => {
