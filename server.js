9757fcbf7f3cd00afac521113a8077b66b059938
Adam Hosman <github@adamsinbox.com>
2019-03-04 01:06:18 +0100

    Remove mock database

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { database } = require('./database');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
